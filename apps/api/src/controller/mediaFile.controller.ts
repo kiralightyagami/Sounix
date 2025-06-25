98a419be53f96285ccb9ec0ee324d0bbea952941
kiralightyagami <crucisgamer98@gmail.com>
2025-06-25 18:45:58 +0530

    forgot to save again

--- apps/api/src/controller/mediaFile.controller.ts
+++ apps/api/src/controller/mediaFile.controller.ts
@@ -1,19 +1,43 @@
 import { Request, Response } from "express";
 import { prisma } from "db/client";
+import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
 
 export class MediaFileController {
     async uploadMediaFile(req: Request, res: Response) {
         const { mediaFileId } = req.params;
-        const { file } = req.body;
+        const { chunkNumber, totalChunks, chunk } = req.body;
 
-        const mediaFile = await prisma.mediaFile.create({
-            data: {
-                id: mediaFileId,
-                name: file.name,
-            },
+        const s3 = new S3Client({
+            region: process.env.AWS_REGION!,
+            credentials: {
+                accessKeyId: process.env.AWS_ACCESS_KEY_ID!,
+                secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!
+            }
         });
-        res.status(200).json(mediaFile);
+        const s3Key = `${mediaFileId}/${chunkNumber}`;
+        await s3.send(new PutObjectCommand({
+            Bucket: process.env.AWS_S3_BUCKET!,
+            Key: s3Key,
+            Body: Buffer.from(chunk, 'base64')
+        }));
+
+       
+        if (chunkNumber === totalChunks) {
+            const mediaFile = await prisma.mediaFile.create({
+                data: {
+                    id: mediaFileId,
+                    s3Key: `${mediaFileId}/complete`,
+                    status: 'UPLOADED'
+                }
+            });
+            res.status(200).json(mediaFile);
+        } else {
+            res.status(200).json({
+                message: `Chunk ${chunkNumber} of ${totalChunks} uploaded successfully`
+            });
+        }
     }
+
 
     async getTranscript(req: Request, res: Response) {
         const { mediaFileId } = req.params;
